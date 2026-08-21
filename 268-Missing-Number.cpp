387dfadd4d7e77de0dce430c56daf4cad2a6be27
kushagra15906 <sandhyasandhya7864@gmail.com>
2026-08-21 11:20:09 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 94.10%] - LeetPush

---
+++ 268-Missing-Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=1;i<nums.size()+1;i++){
+            ans^=i;
+        }
+        for(int i=0;i<nums.size();i++){
+            ans^=nums[i];
+        }
+        return ans;
+    }
+};
